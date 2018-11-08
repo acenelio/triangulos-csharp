5ee36d01079a3e45dee4f9341f376780c9ebce5e
Nelio Alves <acenelio@gmail.com>
2018-11-08 14:09:41 -0200

    Initial commit

---
+++ Course/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            Triangulo x = new Triangulo();
+            Triangulo y = new Triangulo();
+
+            Console.WriteLine("Digite os dados do triangulo X:");
+            x.A = double.Parse(Console.ReadLine());
+            x.B = double.Parse(Console.ReadLine());
+            x.C = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite os dados do triangulo Y:");
+            y.A = double.Parse(Console.ReadLine());
+            y.B = double.Parse(Console.ReadLine());
+            y.C = double.Parse(Console.ReadLine());
+
+            double p = (x.A + x.B + x.C) / 2.0;
+            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+            p = (y.A + y.B + y.C) / 2.0;
+            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            Console.WriteLine("Area de X = " + areaX.ToString("F4"));
+            Console.WriteLine("Area de Y = " + areaY.ToString("F4"));
+
+            if (areaX > areaY) {
+                Console.WriteLine("Maior area: X");
+            }
+            else {
+                Console.WriteLine("Maior area: X");
+            }
+        }
+    }
+}
